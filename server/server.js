c4c000bf0c122d5d4b28ac4488230cea52da3856
Chetan (TCR) Sapkal <68210482+ceotcr@users.noreply.github.com>
2023-12-30 19:48:06 +0530

    Add path module and serve hello.html file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '/public/hello.html'));
 });
 
 app.listen(PORT, () => {
